1e2f06baa67e37133e9f4a7f0e32bd5f001af935
FT-Mystogan <viponlyc@gmail.com>
2023-02-09 10:57:07 +0700

    init

---
+++ src/main/java/com/example/keycloak/IndexController.java
@@ -0,0 +1,54 @@
+package com.example.keycloak;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@RestController
+public class IndexController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public IndexController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping(path = "/overtime_requests/view")
+    public String index() {
+        return "Hello";
+    }
+
+    @PostMapping(path = "/sso-logout", produces = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap logout(@RequestParam("logout_token") String logoutToken) throws Exception {
+
+        String[] splitString = logoutToken.split("\\.");
+        String body = new String(java.util.Base64.getDecoder().decode(splitString[1]));
+
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap bodyMap = mapper.readValue(body, HashMap.class);
+
+        logger.debug("logging out {}", bodyMap.get("sid"));
+
+        jdbcTemplate.update("DELETE FROM `spring_session` " +
+                        "WHERE `PRIMARY_ID` = (SELECT `PRIMARY_ID` FROM `spring_session_attributes` WHERE `ATTRIBUTE_NAME` = ?)",
+                new Object[] { bodyMap.get("sid") });
+
+        return new HashMap(){{
+            put("status", true);
+        }};
+    }
+}
